89233d9e7660c1cccec62ac18a1e84b73a7bc9f9
thomasrichardson439 <richardsonthomas439@gmail.com>
2018-08-31 11:04:47 +0300

    Include theme files into project

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.signin');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
