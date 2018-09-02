8f4b8c8521adfc826f87e9b580e4de9545d2608f
thomasrichardson439 <richardsonthomas439@gmail.com>
2018-09-02 23:03:02 +0300

    Fix auth docs

--- app/Docs/Controllers/Auth/ForgotPasswordController.php
+++ app/Docs/Controllers/Auth/ForgotPasswordController.php
@@ -1,56 +1,58 @@
 <?php
+
 /**
- * @OA\Post(
- *   tags={"password"},
- *   path="api/password/email",
- *   summary="Send reset email link",
- *     @OA\RequestBody(
- *         required=true,
- *         description="Email addres",
- *         @OA\MediaType(
- *             mediaType="multipart/form-data",
- *             @OA\Schema(
- *                 @OA\Property(property="email", format="email", type="string", required={"true"}),
- *            ),
- *         )
- *     ),
- *   @OA\Response(
- *       response=200,
- *       description="successfully sent",
- *       @OA\JsonContent(
- *           @OA\Property(
- *               property="message",
- *               type="string",
- *               example="Reset link was sent"
- *           )
- *      )
- *   ),
- *
- *   @OA\Response(
- *       response="404",
- *       description="Not Found",
- *       @OA\JsonContent(@OA\Property(property="message", example="User with email not found", type="string"))
- *   ),
- *
- *   @OA\Response(
- *       response="422",
- *       description="Validation failed",
- *       @OA\JsonContent(
- *           @OA\Property(property="errors",
- *               type="array",
- *               @OA\Items(
- *                   @OA\Property(
- *                       property="email",
- *                       type="array",
- *                       @OA\Items(type="string", example="Email must be valid email address.")
- *                   )
- *               )
- *           ),
- *       )
- *   ),
- *   @OA\Response(
- *       response="500",
- *       description="Server error"
- *   )
- * )
- */
+*@OA\Post(
+*  tags={"password"},
+*  path="/api/password/email",
+*  summary="Send reset email link",
+*  @OA\RequestBody(
+*    required=true,
+*    description="Email addres",
+*    @OA\MediaType(
+*      mediaType="multipart/form-data",
+*      @OA\Schema(
+*        @OA\Property(property="email", format="email", type="string", required={"true"}),
+*      ),
+*    )
+*  ),
+*  @OA\Response(
+*    response=200,
+*    description="successfully sent",
+*    @OA\JsonContent(
+*      @OA\Property(
+*        property="message",
+*        type="string",
+*        example="Reset link was sent"
+*      )
+*    )
+*  ),
+*
+*  @OA\Response(
+*    response="404",
+*    description="Not Found",
+*    @OA\JsonContent(@OA\Property(property="message", example="User with email not found", type="string"))
+*  ),
+*
+*  @OA\Response(
+*    response="422",
+*    description="Validation failed",
+*    @OA\JsonContent(
+*      @OA\Property(
+*        property="errors",
+*        type="array",
+*        @OA\Items(
+*          @OA\Property(
+*            property="email",
+*            type="array",
+*            @OA\Items(type="string", example="Email must be valid email address.")
+*          )
+*        )
+*      ),
+*    )
+*  ),
+*  @OA\Response(
+*    response="500",
+*    description="Server error"
+*  )
+*)
+*/
--- app/Docs/Controllers/Auth/ResetPasswordController.php
+++ app/Docs/Controllers/Auth/ResetPasswordController.php
@@ -1,65 +1,65 @@
 <?php
+
 /**
- * @OA\Post(
- *   tags={"password"},
- *   path="api/password/change",
- *     security={
- *       {"api_key": {}}
- *     },
- *   summary="Change user password",
- *     @OA\RequestBody(
- *         required=true,
- *         description="Email addres",
- *         @OA\MediaType(
- *             mediaType="multipart/form-data",
- *             @OA\Schema(
- *                 @OA\Property(property="email", format="email", type="string", required={"true"}),
- *            ),
- *         )
- *     ),
- *   @OA\Response(
- *       response=200,
- *       description="successfully login",
- *       @OA\JsonContent(
- *           @OA\Property(
- *               property="auth_token",
- *               type="string"
- *           )
- *      )
- *   ),
- *
- *   @OA\Response(
- *       response="400",
- *       description="Invalid user current password",
- *       @OA\JsonContent(@OA\Property(property="message", example="Invalid user password", type="string"))
- *   ),
- *
- *   @OA\Response(
- *       response="401",
- *       description="Unauthorized",
- *       @OA\JsonContent(@OA\Property(property="message", example="Invalid login or password", type="string"))
- *   ),
- *
- *
- *   @OA\Response(
- *       response="422",
- *       description="Validation failed",
- *       @OA\JsonContent(
- *           @OA\Property(property="errors",
- *               type="array",
- *               @OA\Items(
- *                   @OA\Property(
- *                       property="password",
- *                       type="array",
- *                       @OA\Items(type="string", example="Password is required.")
- *                   )
- *               )
- *           ),
- *       )
- *   ),
- *   @OA\Response(
- *       response="500",
- *       description="Server error"
- *   )
- * )
- */
+*@OA\Post(
+*  tags={"password"},
+*  path="/api/password/change",
+*  security={
+*    {"api_key": {}}
+*  },
+*  summary="Change user password",
+*  @OA\RequestBody(
+*    required=true,
+*    description="Email addres",
+*    @OA\MediaType(
+*      mediaType="multipart/form-data",
+*      @OA\Schema(
+*        @OA\Property(property="password", format="email", type="string", required={"true"}),
+*        @OA\Property(property="new_password", format="password", type="string", required={"true"}),
+*        @OA\Property(property="new_password_confirmation", format="password", type="string", required={"true"}),
+*      ),
+*    )
+*  ),
+*  @OA\Response(
+*    response=200,
+*    description="successfully login",
+*    @OA\JsonContent(
+*      @OA\Property(
+*        property="auth_token",
+*        type="string"
+*      )
+*    )
+*  ),
+*  @OA\Response(
+*    response="400",
+*    description="Invalid user current password",
+*    @OA\JsonContent(@OA\Property(property="message", example="Invalid user password", type="string"))
+*  ),
+*  @OA\Response(
+*    response="401",
+*    description="Unauthorized",
+*    @OA\JsonContent(@OA\Property(property="message", example="Unauthorized.", type="string"))
+*  ),
+*  @OA\Response(
+*    response="422",
+*    description="Validation failed",
+*    @OA\JsonContent(
+*      @OA\Property(
+*        property="errors",
+*        type="array",
+*        @OA\Items(
+*          @OA\Property(
+*            property="password",
+*            type="array",
+*            @OA\Items(type="string", example="Password is required.")
+*          )
+*        )
+*      ),
+*    )
+*  ),
+*  @OA\Response(
+*    response="500",
+*    description="Server error"
+*  )
+*)
+*/
