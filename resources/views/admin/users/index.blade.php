c9601ca3f1cfc026b884cf2ab6136b08161b7c61
thomasrichardson439 <richardsonthomas439@gmail.com>
2018-09-11 17:05:44 +0300

    Change title

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,11 +2,10 @@
 
 @section('content')
 <div class="container-fluid">
-  <h4 class="c-grey-900 mT-10 mB-30">Data Tables</h4>
   <div class="row">
     <div class="col-md-12">
       <div class="bgc-white bd bdrs-3 p-20 mB-20">
-        <h4 class="c-grey-900 mB-20">Bootstrap Data Table</h4>
+        <h4 class="c-grey-900 mB-20">Applicants for Car Flow Inc:</h4>
         <table id="usersTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
               <tr>
