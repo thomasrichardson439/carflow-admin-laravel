b2cc60b0a1ce0e717d0f2c54665d4b9b33920049
thomasrichardson439 <richardsonthomas439@gmail.com>
2018-11-14 01:37:17 +0200

    Doc update

--- app/Docs/Models/CarAvailabilitySlot.php
+++ app/Docs/Models/CarAvailabilitySlot.php
@@ -3,7 +3,7 @@
 /**
  * @OA\Schema(
  *   schema="CarAvailabilitySlot",
- *   @OA\Property(property="availability_type", format="string", type="string"),
+ *   @OA\Property(property="availability_type", format="enum(recurring, one-time)", type="string"),
  *   @OA\Property(property="available_at", format="string", type="string"),
  *   @OA\Property(property="available_at_recurring", format="enum(monday, ..., sunday)", type="string"),
  *   @OA\Property(property="available_hour_from", format="string", type="string"),
