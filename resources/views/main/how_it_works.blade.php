915b3a1f742749ddd67d3726b72b5c1fa347fc19
thomasrichardson439 <richardsonthomas439@gmail.com>
2018-12-03 06:08:38 +0300

    fix updated

--- resources/views/main/how_it_works.blade.php
+++ resources/views/main/how_it_works.blade.php
@@ -12,7 +12,7 @@
 
                     <h1 class="mt-5 pt-5">How Car Flo Works?</h1>
 
-                    <p class="mt-4">Rent an affordable top-quality vehicle for only the hours you plan to drive rideshare. With you never worry about insurance, parking, or vehicle maintenance. And, our system is designed for ultimate driver convenience:</p>
+                    <p class="mt-4">Rent an affordable top-quality vehicle for only the hours you plan to drive rideshare. With Car Flo you never worry about insurance, parking, or vehicle maintenance. And, our system is designed for ultimate driver convenience:</p>
 
                 </div>
 
@@ -42,7 +42,7 @@
                     </div>
 
                     <div class="step">
-                        <h4>3. Register with Car Flow</h4>
+                        <h4>3. Register with Car Flo</h4>
                         <p>Use the app or our <a href="{{url('/main')}}">website page</a> to register. Have these required documents ready to photograph and upload:</p>
                         <ul>
                             <li>TLC License</li>
@@ -51,7 +51,7 @@
                             <li>Debit/Credit Card</li>
                         </ul>
                         <p>We’ll get back with your approval in about four hours.</p>
-                        <p class="text-muted small">Important: Keep the documents you receive back from Car Flow with you while you are driving Car Flow vehicles.</p>
+                        <p class="text-muted small">Important: Keep the documents you receive from Car Flo with you while you are driving Car Flo vehicles.</p>
                     </div>
 
                     <div class="step">
@@ -63,12 +63,12 @@
 
                     <div class="step">
                         <h4>5. Pickup your car</h4>
-                        <p>Use the Car Flow app to quickly communicate any problems to Car Flow. We won’t leave you hanging.</p>
+                        <p>Use the Car Flo app to quickly communicate any problems to Car Flo. We won’t leave you hanging.</p>
                     </div>
 
                     <div class="step">
                         <h4>6. Return your car</h4>
-                        <p>When your time is up, return the car to any Car Flow parking zone, put the keys back in the console and lock the car.</p>
+                        <p>When your time is up, return the car to any Car Flo parking zone, put the keys back in the console and lock the car.</p>
                         <p class="text-muted small">
                             Important: Don’t forget to leave it with the gas tank full and the car clean for the next driver.
                         </p>
@@ -88,7 +88,7 @@
                     <div class="row">
                         <div class="col-12 col-md-8 offset-md-2 offset-0">
                             <div class="buttons my-4">
-                                <a class="btn btn-primary btn-lg btn-block" href="{{url('/main')}}" role="button">Go with “The Flow”</a>
+                                <a class="btn btn-primary btn-lg btn-block" href="{{url('/main')}}" role="button">Go with “The Flo”</a>
                                 <p class="help-text text-center mt-4">
                                     It's free and simple – fill out our registration form and we'll be in touch.
                                 </p>
